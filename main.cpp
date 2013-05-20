7ac55112b5992723e1e8b81feadfdc4fe67346cb
flyc <wit.lwy@gmail.com>
2013-05-20 18:14:04 +0800

    name email test

--- main.cpp
+++ main.cpp
@@ -52,5 +52,8 @@ int main(int argc, char** argv)
 {
 	std::cout<<_LOG_DEBUG_DIR_<<_LOG_DEBUG_OVERDUE<<std::endl;
 	CLogDir dir(_LOG_DEBUG_DIR_, _LOG_DEBUG_OVERDUE);
+	
+	printf("xixi");
+
 	return 0;
 }
